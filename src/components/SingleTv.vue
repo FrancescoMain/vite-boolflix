59d7ada0e574f6f4a3ea076a1c19071cf1654573
Francesco <cesaranofrancescomain@gmail.com>
2022-12-18 12:30:53 +0100

    creato componente single tv

---
+++ src/components/SingleTv.vue
@@ -0,0 +1,82 @@
+<script >
+import { store } from '../store.js'
+export default {
+
+    name: "SingleTv",
+    props: ["info"],
+    data() {
+        return {
+            store,
+        }
+    },
+    computed: {
+        flagInStore() {
+            if (this.store.listOfFlags.includes(this.info.original_language)) {
+                return true
+            }
+
+        },
+        flagType() {
+            return `../../public/img/flag/${this.info.original_language}.png `
+        },
+        stars() {
+            return parseInt(this.info.vote_average / 2)
+        }
+    }
+}
+
+
+</script>
+
+<template>
+
+    <div class="film text-center">
+        <h2>{{ info.name }}</h2>
+        <h3>{{ info.original_name }}</h3>
+        <img :src="store.img + info.backdrop_path" alt="">
+        <img class="flag" v-if="flagInStore" :src="flagType" alt="">
+        <h4 v-else>{{ info.original_language }}</h4>
+        <div class="stars-wrap">
+            <img v-for="star in stars" src="../../public/img/star.png" alt="" class="star">
+        </div>
+
+    </div>
+
+
+</template>
+
+<style lang="scss" scoped>
+.film {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+
+    h2,
+    h3,
+    h5 {
+        text-transform: capitalize;
+
+        &::after {
+            content: '';
+            display: block;
+            width: 40px;
+            height: 1px;
+            margin: 1rem, auto;
+        }
+    }
+
+    img.flag {
+        border-radius: 50%;
+        box-shadow: 0 0 6px 3px rgba(#000, 0.15);
+        width: 20px;
+        display: block;
+
+    }
+
+    img.star {
+        width: 40px;
+    }
+}
+</style>
