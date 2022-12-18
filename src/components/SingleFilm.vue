5f117d5324edfc3df035e7da3a9879d8ae267530
Francesco <cesaranofrancescomain@gmail.com>
2022-12-18 12:04:46 +0100

    aggiunta immagine di copertina più stile

--- src/components/SingleFilm.vue
+++ src/components/SingleFilm.vue
@@ -30,7 +30,8 @@ export default {
     <div class="film text-center">
         <h2>{{ info.title }}</h2>
         <h3>{{ info.original_title }}</h3>
-        <img v-if="flagInStore" :src="flagType" alt="">
+        <img :src="store.img + info.backdrop_path" alt="">
+        <img class="flag" v-if="flagInStore" :src="flagType" alt="">
         <h4 v-else>{{ info.original_language }}</h4>
         <h5>{{ info.vote_average }}</h5>
     </div>
@@ -39,23 +40,33 @@ export default {
 </template>
 
 <style lang="scss" scoped>
-h2,
-h3,
-h5 {
-    text-transform: capitalize;
+.film {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
 
-    &::after {
-        content: '';
-        display: block;
-        width: 40px;
-        height: 1px;
-        margin: 1rem, auto;
+
+    h2,
+    h3,
+    h5 {
+        text-transform: capitalize;
+
+        &::after {
+            content: '';
+            display: block;
+            width: 40px;
+            height: 1px;
+            margin: 1rem, auto;
+        }
     }
-}
 
-img {
-    border-radius: 50%;
-    box-shadow: 0 0 6px 3px rgba(#000, 0.15);
-    width: 20px;
+    img.flag {
+        border-radius: 50%;
+        box-shadow: 0 0 6px 3px rgba(#000, 0.15);
+        width: 20px;
+        display: block;
+
+    }
 }
 </style>
