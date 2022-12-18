4b7603474eeff7ba4a2ff7f4be533538f5154263
Francesco <cesaranofrancescomain@gmail.com>
2022-12-18 13:00:54 +0100

    Creato AppHead

---
+++ src/components/appHead.vue
@@ -0,0 +1,51 @@
+<script >
+import { store } from '../store.js'
+
+export default {
+
+    name: "SearchBar",
+
+
+    data() {
+        return {
+            store,
+        }
+    }
+}
+
+</script>
+
+<template>
+    <div class="wrapper">
+        <h2>BOOLFIX</h2>
+        <div class="row">
+            <div class="col-auto">
+                <input v-model="store.query" class="form-control" placeholder="Cerca un film"
+                    @keyup.enter.prevent="$emit('search')">
+            </div>
+        </div>
+    </div>
+
+
+</template>
+
+<style lang="scss" scoped>
+@use "../styles/partials/variables.scss" as *;
+
+.wrapper {
+    background-color: $bg-color-first ;
+    display: flex;
+    justify-content: space-between;
+
+
+    h2 {
+        padding: 20px;
+        color: $title-color;
+    }
+
+    div.row {
+        padding: 20px
+    }
+}
+</style>
+
